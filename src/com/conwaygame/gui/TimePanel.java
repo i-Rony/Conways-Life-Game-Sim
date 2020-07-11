210e916165649b342cbd30cf4f814471ae64cab5
i_Rony <ronrocks999@gmail.com>
2020-07-11 08:00:30 +0530

    Final Commit

---
+++ src/com/conwaygame/gui/TimePanel.java
@@ -0,0 +1,32 @@
+package com.conwaygame.gui;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TimePanel extends JPanel{
+	
+	private static final long serialVersionUID = 1L;
+	private JLabel timeLabel;
+	public int count=0;
+	
+	public TimePanel(){
+		initializeLayout();
+	}
+
+	private void initializeLayout() {
+		setLayout(new FlowLayout(FlowLayout.CENTER));
+		timeLabel=new JLabel("Generations: 0");
+		add(timeLabel);
+	}
+	
+	public void refreshCounter(){
+		count=0;
+		refresh();
+	}
+	
+	public void refresh(){
+		timeLabel.setText("Generations: "+count++);
+	}
+}
